578a91c1ae3516ef1c2a095069ccba7d06145c48
Eduardo Mendes <dudarts@msn.com>
2025-05-04 12:00:20 -0300

    Add class People and User

---
+++ SevenFoodApp/Model/People.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenFoodApp.Model
+{
+    internal abstract class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public People(string name, string password)
+        {
+            this.Name = name;
+            this.Password = password;
+        }
+    }
+}
---
+++ SevenFoodApp/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenFoodApp.Model
+{
+    internal class User : People
+    {
+        public User(string name, string password) : base(name, password) { }
+
+    }
+}
